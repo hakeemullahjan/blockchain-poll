6e87418d248fc82c4d6a8e26d850cd2ec0f4ba55
hakeemullahjan <hakeemullah.yousufzai@gmail.com>
2020-11-08 21:24:39 +0500

    Contract interaction

---
+++ blockchian/contract_interaction.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.6.6;
+
+contract MyContract {
+    uint256 public myNumber;
+
+    constructor() public {
+        myNumber=12;
+    }
+
+    function setNumber(uint256 _num)  external {
+        myNumber=_num;
+    }
+
+    // function getNumber() public view returns (uint256){
+    //     return myNumber;
+    // }
+}
